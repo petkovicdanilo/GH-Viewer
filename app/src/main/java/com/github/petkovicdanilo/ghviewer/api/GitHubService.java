454b5543af034d9488fa269dce5cab7b53a70511
Danilo <petkovicdanilo97@gmail.com>
2021-07-02 01:29:33 +0200

    Remove unused resources

--- app/src/main/java/com/github/petkovicdanilo/ghviewer/api/GitHubService.java
+++ app/src/main/java/com/github/petkovicdanilo/ghviewer/api/GitHubService.java
@@ -2,13 +2,11 @@ package com.github.petkovicdanilo.ghviewer.api;
 
 import com.github.petkovicdanilo.ghviewer.api.dto.ActivityDto;
 import com.github.petkovicdanilo.ghviewer.api.dto.CreateRepositoryDto;
-import com.github.petkovicdanilo.ghviewer.api.dto.EmailDto;
 import com.github.petkovicdanilo.ghviewer.api.dto.RepositoryDto;
 import com.github.petkovicdanilo.ghviewer.api.dto.RepositorySearchResultDto;
 import com.github.petkovicdanilo.ghviewer.api.dto.UserDto;
 
 import java.util.List;
-import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -18,13 +16,6 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface GitHubService {
-    @GET("repos/{owner}/{repo}/languages")
-    Call<Map<String, Integer>> listLanguages(@Path("owner") String owner,
-                                             @Path("repo") String repo);
-
-    @GET("user/emails")
-    Call<List<EmailDto>> getEmails();
-
     @POST("user/repos")
     Call<RepositoryDto> createRepo(@Body() CreateRepositoryDto createRepo);
 
