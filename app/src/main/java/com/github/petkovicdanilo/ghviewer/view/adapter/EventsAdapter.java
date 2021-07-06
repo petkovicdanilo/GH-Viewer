7184b4a9869cdea8008e1153b0113a69fb938881
Danilo <petkovicdanilo97@gmail.com>
2021-07-06 16:19:33 +0200

    Update events ui

--- app/src/main/java/com/github/petkovicdanilo/ghviewer/view/adapter/EventsAdapter.java
+++ app/src/main/java/com/github/petkovicdanilo/ghviewer/view/adapter/EventsAdapter.java
@@ -1,5 +1,6 @@
 package com.github.petkovicdanilo.ghviewer.view.adapter;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.github.petkovicdanilo.ghviewer.R;
 import com.github.petkovicdanilo.ghviewer.api.dto.EventDto;
+import com.github.petkovicdanilo.ghviewer.api.dto.RepositoryDto;
+import com.github.petkovicdanilo.ghviewer.api.dto.UserDto;
 
 import java.util.List;
 
@@ -47,7 +50,7 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.ViewHolder
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         EventDto event = events.get(position);
 
-        holder.getTextView().setText(event.getType().getValue());
+        holder.getTextView().setText(eventToString(event, holder));
 
         ImageView profileImage = holder.getProfileImage();
         Glide.with(fragment)
@@ -90,5 +93,65 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.ViewHolder
 
     public interface OnEventListener {
         void onEventClick(int position);
+    }
+
+    private String eventToString(EventDto event, ViewHolder viewHolder) {
+        Context context = viewHolder.itemView.getContext();
+        String repositoryName = event.getRepo().getName();
+        String actorLogin = event.getActor().getLogin();
+
+        switch (event.getType()) {
+            case COMMIT_COMMENT_EVENT:
+                return context.getString(R.string.commit_comment_event, actorLogin, repositoryName);
+            case CREATE_EVENT:
+            case DELETE_EVENT:
+                String ref = (String) event.getPayload().get("ref_type");
+                return context.getString(R.string.create_event, actorLogin, ref,
+                        repositoryName);
+            case FORK_EVENT:
+                return context.getString(R.string.fork_event, actorLogin, repositoryName);
+            case GOLLUM_EVENT:
+                return context.getString(R.string.gollum_event, actorLogin, repositoryName);
+            case ISSUE_COMMENT_EVENT:
+                String issueCommentAction = (String) event.getPayload().get("action");
+                return context.getString(R.string.issue_comment_event, actorLogin,
+                        issueCommentAction, repositoryName);
+            case ISSUES_EVENT:
+                String issueAction = (String) event.getPayload().get("action");
+                return context.getString(R.string.issues_event, actorLogin, issueAction,
+                        repositoryName);
+            case MEMBER_EVENT:
+                String memberAction = (String) event.getPayload().get("action");
+                return context.getString(R.string.member_event, actorLogin, memberAction,
+                        repositoryName);
+            case PUBLIC_EVENT:
+                return context.getString(R.string.public_event, actorLogin, repositoryName);
+            case PULL_REQUEST_EVENT:
+                String pullRequestAction = (String) event.getPayload().get("action");
+                return context.getString(R.string.pull_request_event, actorLogin,
+                        pullRequestAction, repositoryName);
+            case PULL_REQUEST_REVIEW_EVENT:
+                String pullRequestReviewAction = (String) event.getPayload().get("action");
+                return context.getString(R.string.pull_request_review_event, actorLogin,
+                        pullRequestReviewAction, repositoryName);
+            case PULL_REQUEST_REVIEW_COMMENT_EVENT:
+                String pullRequestReviewCommentAction = (String) event.getPayload().get("action");
+                return context.getString(R.string.pull_request_review_comment_event, actorLogin,
+                        pullRequestReviewCommentAction, repositoryName);
+            case PUSH_EVENT:
+                return context.getString(R.string.push_event, actorLogin, repositoryName);
+            case RELEASE_EVENT:
+                String releaseAction = (String) event.getPayload().get("action");
+                return context.getString(R.string.release_event, actorLogin, releaseAction,
+                        repositoryName);
+            case SPONSORSHIP_EVENT:
+                String sponsorshipAction = (String) event.getPayload().get("action");
+                return context.getString(R.string.sponsorship_event, actorLogin,
+                        sponsorshipAction, repositoryName);
+            case WATCH_EVENT:
+                return context.getString(R.string.watch_event, actorLogin, repositoryName);
+            default:
+                return "";
+        }
     }
 }
